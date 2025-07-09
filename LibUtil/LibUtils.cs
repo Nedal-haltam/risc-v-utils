e1606f9c2e34bca1cd573f77e7f8fcfbf984146c
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-07-09 20:21:20 +0300

    update interface and separated immediate generation

--- LibUtil/LibUtils.cs
+++ LibUtil/LibUtils.cs
@@ -116,28 +116,37 @@ public static class LibUtils
         public string Funct3 = Funct3;
         public string Funct7 = Funct7;
     }
-    // make it into two separate functions
-    // 1- string imm -> number literal
-    // 2- number literal -> binary string
-    public static string GetImmediateToBin(string imm)
+    // TODO:
+    //      - test the edge case of load immediate `li` and go back to the assembler
+    //      - test all instruction in all cases (specially ones that uses immediate)
+    //      - test shift instructions and ensure that it uses only 5-bits of the oprand
+    public static dynamic StringToNumberLiteral(string imm)
     {
         if (imm.StartsWith("0x"))
         {
             imm = imm[2..];
             if (UInt32.TryParse(imm, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out UInt32 value))
-                return Convert.ToString(value, 2).PadLeft(32, '0');
+                return value;
             Shartilities.Log(Shartilities.LogType.ERROR, $"could not parse immediate `{imm}`\n", 1);
         }
         else
         {
             if (UInt32.TryParse(imm, out UInt32 UnsignedValue))
-                return Convert.ToString(UnsignedValue, 2).PadLeft(32, '0');
+                return UnsignedValue;
             else if (Int32.TryParse(imm, out Int32 SignedValue))
-                return Convert.ToString(SignedValue, 2).PadLeft(32, '0');
+                return SignedValue;
             Shartilities.Log(Shartilities.LogType.ERROR, $"could not parse immediate `{imm}`\n", 1);
         }
         Shartilities.UNREACHABLE("GetImmediate");
         return "BogusAmogus";
+    }
+    public static string NumberLiteralToBinaryString(dynamic NumberLiteral)
+    {
+        return Convert.ToString(NumberLiteral, 2).PadLeft(32, '0');
+    }
+    public static string GetImmediateToBin(string imm)
+    {
+        return NumberLiteralToBinaryString(StringToNumberLiteral(imm));
     }
     public static List<string> GetIM_INIT(List<string> MachinceCodes, List<Instruction> Instructions)
     {
@@ -157,7 +166,7 @@ public static class LibUtils
     {
         return Directive == ".space" || Directive == ".string" || Directive == ".word";
     }
-    public static List<string> GetDM(List<string> DataMemoryValues)
+    public static List<string> ParseDataMemoryValues(List<string> DataMemoryValues)
     {
         List<string> DM_Bytes = [];
         string? Directive = null;
@@ -206,15 +215,15 @@ public static class LibUtils
         }
         return DM_Bytes;
     }
-    public static void print_regs(List<string> regs)
+    public static void PrintRegs(List<string> regs)
     {
-        Console.Write(get_regs(regs).ToString());
+        Console.Write(GetRegs(regs).ToString());
     }
-    public static void print_DM(List<string> DM)
+    public static void PrintDM(List<string> DM)
     {
         Console.Write(GetDM(DM).ToString());
     }
-    public static StringBuilder get_regs(List<string> regs)
+    public static StringBuilder GetRegs(List<string> regs)
     {
         StringBuilder sb = new();
         sb.Append("Register file content : \n");
@@ -222,15 +231,15 @@ public static class LibUtils
         foreach (string n in regs)
         {
             Int32 signed = Int32.Parse(n);
-            UInt32 unsigned = UInt32.Parse(n);
+            UInt32 unsigned = signed < 0 ? (uint)signed : UInt32.Parse(n);
             string temp = $"index = {i++,10} , reg_out : signed = {signed,10} , unsigned = {unsigned,10}\n";
             sb.Append(temp);
         }
         return sb;
     }
-    public static StringBuilder get_DM(List<string> DM)
+    public static StringBuilder GetDM(List<string> DM)
     {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new();
         sb.Append("Data Memory Content : \n");
         int i = 0;
         foreach (string mem in DM)
